d4b19a5232e1b1745a990d6a8e9b3083a18ce5b0
goel-nandini <goelnandini.2005@gmail.com>
2026-07-13 12:06:40 +0530

    models

---
+++ src/models/playlist.model.js
@@ -0,0 +1,26 @@
+import mongoose,{mongo, Schema} from "mongoose";
+const playlistSchema = new Schema({
+    name :{
+        type: String,
+        required: true
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    videos:[{
+        type: Schema.Types.ObjectId,
+        required: "Video"
+    }],
+
+    owner:{
+        type: Schema.Types.ObjectId,
+        required: "User"
+    },
+
+
+
+
+},{timestamps: true})
+
+export const Playlist = mongoose.model("Playlist",playlistSchema)
